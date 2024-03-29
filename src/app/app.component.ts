c0d1d3d428c4ebbde242188aed93499973afbcb7
mtatiana115 <mtatiana115@gmail.com>
2024-03-29 12:18:47 -0500

    feature/translation services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-root',
@@ -8,6 +10,31 @@ import { RouterOutlet } from '@angular/router';
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	private _translateService = inject(TranslateService);
+	private _platformId = inject(PLATFORM_ID);
+
 	title = 'portafolio-frontend';
+	ngOnInit(): void {
+		if (isPlatformBrowser(this._platformId)) {
+			this.translationsConfig();
+		}
+	}
+	translationsConfig() {
+		const supportedLanguages = ['es', 'en'];
+		const savedLanguage = localStorage.getItem('language');
+		const defaultLanguage = this._translateService.getBrowserLang() || '';
+
+		this._translateService.addLangs(supportedLanguages);
+
+		if (savedLanguage && supportedLanguages.includes(savedLanguage)) {
+			this._translateService.setDefaultLang(savedLanguage);
+		} else if (!savedLanguage && supportedLanguages.includes(defaultLanguage)) {
+			this._translateService.setDefaultLang(defaultLanguage);
+			localStorage.setItem('language', defaultLanguage);
+		} else {
+			this._translateService.setDefaultLang('en');
+			localStorage.setItem('language', 'en');
+		}
+	}
 }
