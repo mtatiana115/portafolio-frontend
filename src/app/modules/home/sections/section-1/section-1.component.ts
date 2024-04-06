1380b61de44d465850a060d2696fd56da3fe7fd1
mtatiana115 <mtatiana115@gmail.com>
2024-04-05 23:20:36 -0500

    feature/ new section added

---
+++ src/app/modules/home/sections/section-1/section-1.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+// Services
+import { TranslateModule } from '@ngx-translate/core';
+import { ButtonComponent } from 'src/app/shared/ui/molecules/button/button.component';
+
+@Component({
+	selector: 'app-section-1',
+	standalone: true,
+	imports: [TranslateModule, ButtonComponent],
+	templateUrl: './section-1.component.html',
+	styleUrl: './section-1.component.scss',
+})
+export class Section1Component {
+	aboutMeClick() {
+		console.log('Hola');
+	}
+}
