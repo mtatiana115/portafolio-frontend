b7298eda02478a07a374e5af636d29f78a2aa605
mtatiana115 <mtatiana115@gmail.com>
2024-04-05 23:18:22 -0500

    feature/ structure changed

--- src/app/layout/sections/header/header.component.ts
+++ src/app/layout/sections/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 // Components
 import { DesktopMenuComponent } from '@layout/components/desktop-menu/desktop-menu.component';
+import { SwitchLanguageComponent } from '@layout/components/switch-language/switch-language.component';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [DesktopMenuComponent],
+	imports: [DesktopMenuComponent, SwitchLanguageComponent],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })
