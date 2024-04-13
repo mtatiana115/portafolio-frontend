abc77bc7ba001cb499f3c7cb468c3f0793bd8dd7
Tatiana Maldonado <mtatiana115@gmail.com>
2024-04-13 13:49:13 -0500

    feature/privacy policy module

--- src/app/layout/sections/footer/footer.component.ts
+++ src/app/layout/sections/footer/footer.component.ts
@@ -1,10 +1,14 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+// Services
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-footer',
 	standalone: true,
-	imports: [DatePipe],
+	imports: [DatePipe, RouterLink, TranslateModule],
 	templateUrl: './footer.component.html',
 	styleUrl: './footer.component.scss',
 })
--- src/app/modules/privacy-policy/page/privacy-policy-page.component.ts
+++ src/app/modules/privacy-policy/page/privacy-policy-page.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Subscription } from 'rxjs';
+
+// Services
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-privacy-policy-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './privacy-policy-page.component.html',
-  styleUrl: './privacy-policy-page.component.scss'
+	selector: 'app-privacy-policy-page',
+	standalone: true,
+	imports: [],
+	templateUrl: './privacy-policy-page.component.html',
+	styleUrl: './privacy-policy-page.component.scss',
 })
-export class PrivacyPolicyPageComponent {
+export class PrivacyPolicyPageComponent implements OnInit, OnDestroy {
+	private _document = inject(DOCUMENT);
+	private _platformId = inject(PLATFORM_ID);
+	private _translateService = inject(TranslateService);
 
+	content: any = {};
+	subscriptions: Subscription[] = [];
+
+	ngOnInit(): void {
+		if (isPlatformBrowser(this._platformId)) {
+			this._document.documentElement.scrollTop = 0;
+			this.getTranslations();
+		}
+	}
+
+	ngOnDestroy(): void {
+		if (isPlatformBrowser(this._platformId)) {
+			this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+		}
+	}
+
+	getTranslations() {
+		this.subscriptions.push(
+			this._translateService.stream('PRIVACYPOLICY.content').subscribe({
+				next: (response) => {
+					this.content = response;
+				},
+			})
+		);
+	}
 }
