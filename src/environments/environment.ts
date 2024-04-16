521dd0425490c8f1de6c66be7b7df3b48b2f136a
mtatiana115 <mtatiana115@gmail.com>
2024-04-15 23:03:04 -0500

    feature/environments created

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+	EMAIL_JS_SERVICE: import.meta.env['NG_APP_EMAIL_JS_SERVICE'],
+	EMAIL_JS_TEMPLATE: import.meta.env['NG_APP_EMAIL_JS_TEMPLATE'],
+	EMAIL_JS_KEY: import.meta.env['NG_APP_EMAIL_JS_KEY'],
+};
